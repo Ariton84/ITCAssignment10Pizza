38440b3484ddee4b9e0c1f8aa48575adeece2e17
Ariton84 <vanver@gmail.com>
2016-12-18 23:18:47 -0800

    Add files via upload

---
+++ App_Code/MealOrder.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for MealOrder
+/// </summary>
+public class MealOrder
+{
+    public List<string> Sizes { set; get; }
+    public List<string> Toppings { set; get; }
+    public MealOrder()
+    {
+        Sizes = new List<string>();
+        Toppings = new List<string>();
+    }
+
+    public void AddSizes(string item)
+    {
+        Sizes.Add(item);
+    }
+
+    public void AddToppings(string item)
+    {
+        Toppings.Add(item);
+    }
+
+    public double Calculatetotal()
+    {
+        double total = 0;
+        foreach (string size in Sizes)
+        {
+            if (size == "Small  12in.")
+            {
+                total += 9.99;
+            }
+            else if(size == "Medium  14in.")
+            {
+                total += 11.99;
+            }
+            else if (size == "Large  18in.")
+            {
+                total += 15.99;
+            }
+        }
+        foreach (string topping in Toppings)
+            {
+                total += 1.00;
+            }
+        return total;
+    }
+}
---
+++ Default.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            PopulateSizes();
+            PopulateToppings();
+        }
+    }
+
+    protected void PopulateSizes()
+    {
+        string[] size = {"Small  12in.", "Medium  14in.", "Large  18in."};
+        CBLSizes.DataSource = size;
+        CBLSizes.DataBind();
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        MealOrder meal = new MealOrder();
+        foreach (ListItem item in CBLSizes.Items)
+        {
+            if (item.Selected)
+            {
+                meal.AddSizes(item.Text);
+            }
+        }
+        foreach (ListItem item in CBLToppings.Items)
+        {
+            if (item.Selected)
+            {
+                meal.AddToppings(item.Text);
+            }
+        }
+        Session["Meal"] = meal;
+        Response.Redirect("Receipt.aspx");
+    }
+
+    protected void PopulateToppings()
+    {
+        string[] topping = {"Pepperoni", "Sausage", "Pinapple", "Canadian Bacon", "Extra Cheese"};
+        CBLToppings.DataSource = topping;
+        CBLToppings.DataBind();
+    }
+}
